11812fcc1ea3704ccea42762a6a757c126de40a3
Pradip Dharam <pradip.dharam@gmail.com>
2024-05-26 19:52:21 +0530

    Used PermissionFactory in search method of SearchProductAPI. Allow user to search the product only if user is permitted to search.

--- api/search_product_api.py
+++ api/search_product_api.py
@@ -1,6 +1,7 @@
 from typing import Final, List
 
-from data import FilterDetails, Product
+from data import FilterDetails, Product, User
+from permission import PermissionFactory
 from searcher import ProductSearcher
 
 
@@ -10,5 +11,9 @@ class SearchProductAPI:
     def __init__(self, product_searcher):
         self.__product_searcher = product_searcher
 
-    def search(self, product_name: str, filter_details: FilterDetails) -> List[Product]:
+    def search(self, product_name: str, filter_details: FilterDetails,
+               user: User) -> List[Product]:
+        permission = PermissionFactory.get_search_permission(user)
+        if not permission.is_permitted():
+            raise RuntimeError("Search request not permitted!")
         return ProductSearcher.search_products(product_name, filter_details)
